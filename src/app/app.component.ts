623c2f43aca9e96ab070ecba1813dd4c5f865a0b
Paulo Eduardo <eduardo@MBP-de-Paulo.lan>
2020-05-12 10:42:54 -0300

    Adicionando tabela de dados lançamentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'algamoney-ui';
+  lancamentos = [
+    { tipo: 'DESPESA', descricao: 'Teclado', dataVencimento: '30/06/2020',
+      dataPagamento: null, valor: 100.32, pessoa: 'Ibyte'},
+    { tipo: 'RECEITA', descricao: 'Salário', dataVencimento: '30/06/2020',
+      dataPagamento: null, valor: 10000.32, pessoa: 'Ibyte'},
+    { tipo: 'DESPESA', descricao: 'Teclado', dataVencimento: '30/06/2020',
+      dataPagamento: null, valor: 100.32, pessoa: 'Ibyte'},
+    { tipo: 'DESPESA', descricao: 'Teclado', dataVencimento: '30/06/2020',
+      dataPagamento: null, valor: 100.32, pessoa: 'Ibyte'}
+  ];
 }
